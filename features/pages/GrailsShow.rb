6d530dd28c2bc025a1d84be58d99c6f420e52486
Tom <tyeager@manifestcorp.com>
2018-04-02 14:24:13 -0400

    Green on creating, reading, comments, working on red

--- features/pages/GrailsShow.rb
+++ features/pages/GrailsShow.rb
@@ -2,14 +2,14 @@ class GrailsShow < BasePage
 
   div(:display_comments, :id => 'displayCommentTextBox')
   text_field(:name, :id => 'name')
-  text_area(:comment,:id => 'comment')
+  text_area(:comment,:id => 'commentText')
 =begin
   button(:save, :id => 'save')
 =end
   button(:Show, :id => 'ShowPost')
   h1(:title, :id => 'title')
   h3(:body ,:id => 'body')
-  list_item(:comment_item, :class => 'commentItem')
+  unordered_list(:commentsList, :id => 'commentsList')
 
   def display_comments
     self.display_comments_element.when_present.click
--- features/step_definitions/blog_comments.rb
+++ features/step_definitions/blog_comments.rb
@@ -1,8 +1,9 @@
 Given(/^I am reading a blog post from my favorite blogger$/) do
 
   visit GrailsBlogHome
-  @current_page.topPostLink
+  @current_page.topPost
   on_page GrailsShow
+  @browser.button(:id => "ShowPost").click
 
 end
 When(/^I log into the blog$/) do
@@ -17,11 +18,11 @@ When(/^I add my genius comment to the blog post$/) do
   @data = {comment: 'Hooray for Earth!'}
   @current_page.display_comments
   @current_page.populate_page_with @data
-  @current_page.save
+  @browser.button(:id => "save").click
 end
 
 Then(/^my genius comment is at the top of the blog post comments$/) do
-  expect(@current_page.comment_item).to include(@data[:comment])
+  expect(@current_page.commentsList).to include(@data[:comment])
 end
 
 
@@ -29,6 +30,6 @@ Then(/^I should see comments left by other readers$/) do
 
   @data = { name: 'tyeager', comment: 'Hooray for Earth!'}
 
-  expect(@current_page.comment_item).to include(@data[:comment])
+  expect(@current_page.commentsList).to include(@data[:comment])
 end
 
